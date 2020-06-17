cc8e5ce91dfaf46784716134805d05be14440679
kinken <yuenkinken@icloud.com>
2020-06-17 17:10:02 +0800

    MRC block Malloc

--- 存储域/MRC/Malloc/block/ViewController.m
+++ 存储域/MRC/Malloc/block/ViewController.m
@@ -15,19 +15,8 @@ typedef void(^blk_t)(void);
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //情况1 block中捕获使用局部变量
-    //大多数情况下编译器做了判断，自动插入copy的代码将__NSStackBlock__变成__NSMallocBlock__
+    //block捕获了自动变量，手动调用copy方法
     int val = 1;
-    void(^block)(void) = ^(void) {
-        NSLog(@"block invoked");
-        NSLog(@"val : %d", val);
-    };
-    NSLog(@"block : %@",block);
-    
-    //在栈上生成的block并且捕获自动变量,为__NSStackBlock__
-    NSLog(@"%@",^{NSLog(@"val : %d",val);});
-    
-    //情况2 对__NSStackBlock__进行copy操作后，成为__NSMallocBlock__
     NSLog(@"%@",[^{NSLog(@"val : %d",val);} copy]);
 }
 
