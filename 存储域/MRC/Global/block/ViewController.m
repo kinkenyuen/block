f08f8b1a40dbf6816d77f7e1f99ddf2c894dcc5f
kinken <yuenkinken@icloud.com>
2020-06-17 17:02:18 +0800

    MRC block Global

--- 存储域/MRC/Global/block/ViewController.m
+++ 存储域/MRC/Global/block/ViewController.m
@@ -8,12 +8,9 @@
 
 #import "ViewController.h"
 
-//情况1 block定义在全局区域
-void(^block)(void) = ^(void) {
-    NSLog(@"global block");
+void(^global_Block)(void) = ^(void) {
 };
 
-//全局变量
 int global_val = 1;
 static int static_global_val = 2;
 
@@ -22,26 +19,19 @@ static int static_global_val = 2;
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    //情况2 不捕获自动变量
-//    void(^block)(void) = ^(void) {
-//        NSLog(@"block invoked");
-//    };
-    
-//    void(^block)(void) = ^(void) {
-//        NSLog(@"block invoked");
-//        NSLog(@"global_val : %d", global_val);
-//        NSLog(@"static_global_val : %d", static_global_val);
-//    };
-    
-    //静态局部变量
+    //情况1 block定义在全局区
+    NSLog(@"%@",global_Block);
+
+    //情况2 block没有捕获自动变量
+    int val = 1;
     static int static_val = 2;
     void(^block)(void) = ^(void) {
-        NSLog(@"block invoked");
-        NSLog(@"static_val : %d", static_val);
+        NSLog(@"global_val : %d",global_val);
+        NSLog(@"static_global_val : %d",static_global_val);
+        NSLog(@"static_val : %d",static_val);
     };
+    NSLog(@"%@",block);
     
-    block();
 }
-
 
 @end
