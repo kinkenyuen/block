c67fe2c5a37923bef4270779a06772ead7041962
kinken <yuenkinken@icloud.com>
2020-06-17 15:56:07 +0800

    ARC block malloc

---
+++ 存储域/ARC/Malloc/block/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  block
+//
+//  Created by kinken on 2020/6/17.
+//  Copyright © 2020 kinken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 存储域/ARC/Malloc/block/ViewController.m
@@ -0,0 +1,34 @@
+//
+//  ViewController.m
+//  block
+//
+//  Created by kinken on 2020/6/17.
+//  Copyright © 2020 kinken. All rights reserved.
+//
+
+#import "ViewController.h"
+
+typedef void(^blk_t)(void);
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //情况1 block中捕获使用局部变量
+    //大多数情况下编译器做了判断，自动插入copy的代码将__NSStackBlock__变成__NSMallocBlock__
+    int val = 1;
+    void(^block)(void) = ^(void) {
+        NSLog(@"block invoked");
+        NSLog(@"val : %d", val);
+    };
+    NSLog(@"block : %@",block);
+    
+    //在栈上生成的block为__NSStackBlock__
+    NSLog(@"%@",^{NSLog(@"val : %d",val);});
+    
+    //情况2 对__NSStackBlock__进行copy操作后，成为__NSMallocBlock__
+    NSLog(@"%@",[^{NSLog(@"val : %d",val);} copy]);
+}
+
+@end
